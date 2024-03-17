6da19afd6cb18daedb5a100605308935f52e19cb
Zi Chen Hu <lolchen2012@gmail.com>
2024-03-16 21:43:12 -0400

    docs: ignore /rando folder

--- backend/app/nada.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-
-from config.loader import db_uri
-
-try:
-    engine = create_engine(db_uri)
-    print("Database engine created successfully!")
-except Exception as e:
-    print(f"Failed to create database engine: {e}")
